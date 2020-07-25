c566772c35c84c2d97b4c00248c964e91e745d1e
Yusuf Jolaoso <jolaosoyusuf@gmail.com>
2020-07-25 15:28:18 +0100

    Update: modularized the product summary into a component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,49 +1,13 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">
-                        Project Title
-                    </span>
-                    <p>
-                        Posted By Jolly Y
-                    </p>
-                    <p className="grey-text">
-                        25th July, 3pm
-                    </p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">
-                        Project Title
-                    </span>
-                    <p>
-                        Posted By Jolly Y
-                    </p>
-                    <p className="grey-text">
-                        25th July, 3pm
-                    </p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">
-                        Project Title
-                    </span>
-                    <p>
-                        Posted By Jolly Y
-                    </p>
-                    <p className="grey-text">
-                        25th July, 3pm
-                    </p>
-                </div>
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     );
 }
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">
+                    Project Title
+                </span>
+                <p>
+                    Posted By Jolly Y
+                </p>
+                <p className="grey-text">
+                    25th July, 3pm
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectSummary;
